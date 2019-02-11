a8cb49824a283926e88e89df4cede1743b8af73d
chetan <sidharthaupreti@.com>
2019-02-11 10:01:26 +0530

    9th commit

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 8080);
+var server_port = process.env.YOUR_PORT || process.env.PORT || 8000;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+app.listen(server_port || server_host || 8080);
 const path = require('path');
 app.get('/*', function(req, res){
   res.sendFile(path.join(__dirname + '/dist/front/index.html'));
