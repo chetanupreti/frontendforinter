eb04e3f2039e19fd29f8a8576764926d2b704641
chetan <sidharthaupreti@.com>
2019-02-12 20:53:26 +0530

    23th

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const app = express();
 app.use(express.static(__dirname + '/dist/front'));
-app.listen(process.env.PORT   || 8080,()=>{
-  console.log('Server listening on port 8080');
+app.listen(process.env.PORT || 8080,()=>{
+    console.log('Server listening');
 });
 const path = require('path');
 app.get('/*', function(req, res){
   res.sendFile(path.join(__dirname + '/dist/front/index.html'));
 });
-
-
